ce3a8abacec7f8b93f3a38ca3f97fff361cd1429
Al-Moatasem <accmmsherif@gmail.com>
2023-02-18 20:02:55 +0200

    executing migration files in alembic

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 RUN_HOST=${APP_HOST:-0.0.0.0}
 RUN_PORT=${APP_PORT:-8000}
 
+/opt/venv/bin/alembic upgrade heads
+
 /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "${RUN_HOST}:${RUN_PORT}" src.main:app --reload
