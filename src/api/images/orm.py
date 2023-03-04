cd1f6ec504679411fae8052830d41f56151496bb
Al-Moatasem <accmmsherif@gmail.com>
2023-03-05 01:40:57 +0200

    detected objects table - ORM

--- src/api/images/orm.py
+++ src/api/images/orm.py
@@ -1,6 +1,15 @@
 from datetime import datetime
 
-from sqlalchemy import TIMESTAMP, Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import (
+    TIMESTAMP,
+    Boolean,
+    Column,
+    DateTime,
+    Float,
+    ForeignKey,
+    Integer,
+    String,
+)
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 
@@ -29,7 +38,33 @@ class UploadedImage(Base, MixinTimestamp):
             "file_path": self.file_path,
             "hash_value": self.hash_value,
             "size_bytes": self.size_bytes,
-            "original_file_name": self.original_file_name,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
         }
+
+
+class ObjectClasses(Base, MixinTimestamp):
+    __tablename__ = "object_classes"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    # should be replaced with a relationship to a dedicated table
+    ml_model_name = Column(String, nullable=False)
+    ml_model_version = Column(String, nullable=False)
+    ml_class_id = Column(Integer)
+    ml_class_name = Column(String)
+
+
+class DetectedObjects(Base, MixinTimestamp):
+    __tablename__ = "detected_objects"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    user_id = Column(Integer, ForeignKey(UserModel.id), nullable=False)
+    uploaded_image_id = Column(Integer, ForeignKey(UploadedImage.id), nullable=False)
+    object_class_id = Column(Integer, nullable=False)
+    confidence = Column(Float, nullable=False)
+    left = Column(Integer, nullable=False)
+    top = Column(Integer, nullable=False)
+    width = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+    is_correct = Column(Boolean, nullable=True)
+    deleted_at = Column(DateTime, nullable=True)
