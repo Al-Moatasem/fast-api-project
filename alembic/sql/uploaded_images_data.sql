783550652ea7e5e34df3e68b2f41b5ad7409ca1d
Al-Moatasem <accmmsherif@gmail.com>
2023-03-05 01:39:57 +0200

    sample data for the uploaded images

---
+++ alembic/sql/uploaded_images_data.sql
@@ -0,0 +1,28 @@
+INSERT INTO public.uploaded_images 
+    (user_id, file_name, file_path, hash_value, size_bytes) 
+VALUES 
+    ( -2, 'shabrway_01.jpg', 'C:\shabrway_01.jpg', 'hash_qwe', 123),
+    ( -2, 'shabrway_02.jpg', 'C:\shabrway_02.jpg', 'hash_wer', 123),
+    ( -2, 'shabrway_03.jpg', 'C:\shabrway_03.jpg', 'hash_ert', 123),
+    ( -2, 'shabrway_04.jpg', 'C:\shabrway_04.jpg', 'hash_rty', 123),
+    ( -2, 'shabrway_05.jpg', 'C:\shabrway_05.jpg', 'hash_qaz', 123),
+    ( -2, 'shabrway_06.jpg', 'C:\shabrway_06.jpg', 'hash_zaq', 123),
+    ( -3, 'gad_01.jpg', 'C:\gad_01.jpg', 'hash_qwe', 123),
+    ( -3, 'gad_02.jpg', 'C:\gad_02.jpg', 'hash_wer', 123),
+    ( -3, 'gad_03.jpg', 'C:\gad_03.jpg', 'hash_ert', 123),
+    ( -3, 'gad_04.jpg', 'C:\gad_04.jpg', 'hash_rty', 123),
+    ( -3, 'gad_05.jpg', 'C:\gad_05.jpg', 'hash_qaz', 123),
+    ( -3, 'gad_06.jpg', 'C:\gad_06.jpg', 'hash_zaq', 123),
+    ( -4, 'sabry_01.jpg', 'C:\sabry_01.jpg', 'hash_qwe', 123),
+    ( -4, 'sabry_02.jpg', 'C:\sabry_02.jpg', 'hash_wer', 123),
+    ( -4, 'sabry_03.jpg', 'C:\sabry_03.jpg', 'hash_ert', 123),
+    ( -4, 'sabry_04.jpg', 'C:\sabry_04.jpg', 'hash_rty', 123),
+    ( -4, 'sabry_05.jpg', 'C:\sabry_05.jpg', 'hash_qaz', 123),
+    ( -4, 'sabry_06.jpg', 'C:\sabry_06.jpg', 'hash_zaq', 123),
+    ( -5, 'samar_01.jpg', 'C:\samar_01.jpg', 'hash_qwe', 123),
+    ( -5, 'samar_02.jpg', 'C:\samar_02.jpg', 'hash_wer', 123),
+    ( -5, 'samar_03.jpg', 'C:\samar_03.jpg', 'hash_ert', 123),
+    ( -5, 'samar_04.jpg', 'C:\samar_04.jpg', 'hash_rty', 123),
+    ( -5, 'samar_05.jpg', 'C:\samar_05.jpg', 'hash_qaz', 123),
+    ( -5, 'samar_06.jpg', 'C:\samar_06.jpg', 'hash_zaq', 123)
+;
